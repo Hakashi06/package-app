c48ba4702a4509cd78ed5af75eff147cc40096d6
hakashi <hakashi.cse@gmail.com>
2025-09-04 23:26:09 +0700

    fix: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src/renderer'),
-  plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
-  build: {
-    outDir: path.resolve(__dirname, 'dist/renderer'),
-    emptyOutDir: true,
-  },
-})
-
+    base: './',
+    root: path.resolve(__dirname, 'src/renderer'),
+    plugins: [react()],
+    server: {
+        port: 5173,
+        strictPort: true,
+    },
+    build: {
+        outDir: path.resolve(__dirname, 'dist/renderer'),
+        emptyOutDir: true,
+    },
+});
